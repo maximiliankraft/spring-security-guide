276c3ac2bdf8f914e1226dd318767632592f5054
Maximilian Kraft <kraft@spengergasse.at>
2022-12-28 11:28:00 +0100

    added possibility to change the password of a user - tests pending

--- src/main/java/at/maxkraft/restsec/configuration/JPAUserDetailsManager.java
+++ src/main/java/at/maxkraft/restsec/configuration/JPAUserDetailsManager.java
@@ -3,21 +3,26 @@ package at.maxkraft.restsec.configuration;
 import at.maxkraft.restsec.entity.UserEntity;
 import at.maxkraft.restsec.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
 import org.hibernate.NotYetImplementedFor6Exception;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @AllArgsConstructor
 @Component
 public class JPAUserDetailsManager implements UserDetailsManager {
 
-
-
     UserRepository userRepository;
-
-    // Optional<UserDetails> currentUser = Optional.empty();
+    SecurityContextHolderStrategy securityContextHolderStrategy;
 
     @Override
     public void createUser(UserDetails user) {
@@ -25,7 +30,6 @@ public class JPAUserDetailsManager implements UserDetailsManager {
         if (user instanceof UserEntity){
             userRepository.save((UserEntity)user);
         }
-
     }
 
     @Override
@@ -40,7 +44,19 @@ public class JPAUserDetailsManager implements UserDetailsManager {
 
     @Override
     public void changePassword(String oldPassword, String newPassword) {
-        throw new NotYetImplementedFor6Exception("changePassword");
+
+        // based on implementation of `InMemoryUserDetailsManager`
+        Authentication currentUser = this.securityContextHolderStrategy.getContext().getAuthentication();
+
+        var user = userRepository.findByUsername(currentUser.getName());
+
+        if (user.isPresent()){
+            var currentPassword = user.get().getPassword();
+            if (Objects.equals(new BCryptPasswordEncoder().encode(oldPassword), currentPassword)){
+                user.get().setPassword(newPassword); // setter `setPassword` also encrypts the password
+                userRepository.save(user.get());
+            }
+        }
     }
 
     @Override
